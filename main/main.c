ffa92c188a69e250f7c879372bfb4da5e73aa2c7
Le Hoan <nightmare3198@gmail.com>
2026-05-29 17:44:54 +0700

    Add INA226 driver (alternate to MAX17048)

--- main/main.c
+++ main/main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include "esp_log.h"
 #include "i2c_manager.h"
-#include "bh1750.h"
-#include "bme280.h"
-#include "bmp280.h"
+
 
 void app_main(void)
 {
-    i2c_init();
-
-    xTaskCreate(bmp280_app_test, "bmp280_task", 2048, NULL, 5, NULL);
-    xTaskCreate(bh1750_app_test, "bh1750_task", 2048, NULL, 5, NULL);
 }
 
