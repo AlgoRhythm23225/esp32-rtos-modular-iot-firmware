21ab36a4779d6232a2556f52d7fb651557e289e5
Huy Van <thevirtuoso208110@gmail.com>
2026-05-29 14:18:12 +0700

    implement and complete ble_manager v1

--- main/main.c
+++ main/main.c
@@ -2,11 +2,12 @@
 
 #include "esp_log.h"
 #include "nvs_manager.h"
-
+#include "ble_manager.h"
 static const char *TAG = "APP_MAIN";
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "System init");
     nvs_manager_init();
+    ble_manager_init();
 }
